7e1386942e5b34196149ae4e43da74a281d1af4c
FrancoBonzi <bon.franco12@gmail.com>
2024-10-04 20:20:35 -0300

    class Vouchers(dominio),class ClientesNegocio(negocio) y class VouchersNegocio(negocio)

---
+++ negocio/ClientesNegocio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ClientesNegocio
+    {
+        public Clientes ObtenerClientes(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            Clientes clientes = new Clientes();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id,Documento,Nombre,Email,Direccion,Ciudad,CP FROM CLIENTES WHERE Id=@id ");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+
+                if (datos.Lector.Read())
+                {
+                    clientes.IdCliente = (int)datos.Lector["Id"];
+                    clientes.Documento = (string)datos.Lector["Documento"];
+                    clientes.Nombre = (string)datos.Lector["Nombre"];
+                    clientes.Apellido = (string)datos.Lector["Apellido"];
+                    clientes.Direccion = (string)datos.Lector["Direccion"];
+                    clientes.Ciudad = (string)datos.Lector["Ciudad"];
+                    clientes.CP = (int)datos.Lector["CP"];
+                }
+                return clientes;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ negocio/VouchersNegocio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class VouchersNegocio
+    {
+        public Vouchers obtenerVouchers(string CV)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            Vouchers vouchers = new Vouchers();
+
+            try
+            {
+                datos.setearConsulta("SELECT CodigoVoucher,IdCliente,FechaCanje,IdArticulo FROM Vouchers WHERE CodigoVoucher LIKE @Codigo");
+                datos.setearParametro("@Codigo","%"+ CV+"%");
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    vouchers.CodigoVoucher = (string)datos.Lector["IdCliente"];
+
+                    if (datos.Lector["IdCliente"] is DBNull)
+                    {
+                        vouchers.IdCliente = (int)datos.Lector["IdCliente"];
+                        vouchers.FechaCanje = (DateTime)datos.Lector["FechaCanje"];
+                        vouchers.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    }
+                }
+                return vouchers;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
