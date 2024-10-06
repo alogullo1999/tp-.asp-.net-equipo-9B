68c37a693a93c98c4ecb4e2ed439bcfb2d5e3234
alogullo1999 <adriel_logullo@hotmail.com>
2024-10-06 18:59:21 -0300

    Agregamos la pagina SeleccionarPremio y cambiamos el FormularioClientes
    Se realiza el insert en la base de datos en el form, y visualizamos imagenes

--- Promo Web/SeleccionarPremio.aspx
+++ Promo Web/SeleccionarPremio.aspx
@@ -1,26 +1,58 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SeleccionarPremio.aspx.cs" Inherits="Promo_Web.SeleccionarPremio" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SeleccionarPremio.aspx.cs" Inherits="Promo_Web.SeleccionarPremio" %>
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>Listado de Artículos</title>
+    <title>Lista de Productos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
-        <asp:Repeater ID="rptPremios" runat="server">
-            <ItemTemplate>
-                <div class="articulo">
-                    <h3><%# Eval("Nombre") %></h3>
-                    <img src='<%# Eval("ImagenUrl") %>' alt='<%# Eval("Nombre") %>' style="width: 200px; height: 200px;" />
-                    <p><%# Eval("Descripcion") %></p>
-                    <p><strong>Precio:</strong> <%# Eval("Precio", "{0:C}") %></p>
-                    <asp:Button ID="btnSeleccionar" runat="server" Text="Seleccionar" CommandArgument='<%# Eval("IdArticulo") %>' OnClick="btnEjemplo_Click" />
-                   
-                </div>
-                <hr />
-            </ItemTemplate>
-        </asp:Repeater>
-        <asp:Label ID="lblMessage2" runat="server" ForeColor="Red" />
+        <div class="container mt-4">
+            <h2>Lista de Productos</h2>
+
+            <!-- Repeater para mostrar los productos -->
+            <asp:Repeater ID="rptPremios" runat="server">
+                <HeaderTemplate>
+                    <div class="row">
+                        <div class="col-3"><strong>Imagen</strong></div>
+                        <div class="col-3"><strong>Nombre</strong></div>
+                        <div class="col-3"><strong>Descripción</strong></div>
+                        <div class="col-3 text-end"><strong>Seleccionar</strong></div>
+                    </div>
+                </HeaderTemplate>
+
+                <ItemTemplate>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <!-- Mostrar la imagen del producto -->
+                            <asp:Image ID="imgProducto" runat="server" ImageUrl='<%# Eval("ImagenUrl") %>' CssClass="img-fluid" />
+                        </div>
+                        <div class="col-3">
+                            <!-- Mostrar el nombre del producto -->
+                            <%# Eval("Nombre") %>
+                        </div>
+                        <div class="col-3">
+                            <!-- Mostrar la descripción del producto -->
+                            <%# Eval("Descripcion") %>
+                        </div>
+                        <div class="col-3 text-end">
+                            <!-- Usar RadioButton para seleccionar solo uno dentro del mismo grupo -->
+                            <asp:RadioButton ID="rdbSeleccionar" runat="server" GroupName="Premios" />
+                            <asp:HiddenField ID="hfPremioID" runat="server" Value='<%# Eval("PremioID") %>' />
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+
+            <!-- Botón para enviar el premio seleccionado -->
+            <div class="mt-4">
+                <asp:Button ID="btnSeleccionarPremio" runat="server" CssClass="btn btn-primary" Text="Seleccionar Premio" OnClick="btnSeleccionarPremio_Click" />
+            </div>
+
+            <!-- Etiqueta para mostrar mensajes -->
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+        </div>
     </form>
 </body>
 </html>
--- Promo Web/SeleccionarPremio.aspx.cs
+++ Promo Web/SeleccionarPremio.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using negocio; 
 using dominio;
-using negocio;
+using System.Collections.Generic;
 
 namespace Promo_Web
 {
@@ -12,45 +14,69 @@ namespace Promo_Web
         {
             if (!IsPostBack)
             {
-                CargarPremios();
+                CargarProductos();
             }
         }
 
-        private void CargarPremios()
+        private void CargarProductos()
         {
+            // Crear una instancia de AccesoDatos o la clase ClientesNegocio para obtener los datos
             AccesoDatos datos = new AccesoDatos();
             try
             {
-                datos.setearConsulta("SELECT a.Id AS PremioID, a.Nombre, a.Descripcion, a.Precio, i.ImagenUrl FROM Articulos a INNER JOIN Imagenes i ON a.Id = i.IdArticulo");
+                // Establecer la consulta SQL para obtener los productos
+                datos.setearConsulta("SELECT a.Id AS PremioID, a.Nombre, a.Descripcion, MIN(i.ImagenUrl) AS ImagenUrl FROM Articulos a INNER JOIN Imagenes i ON a.Id = i.IdArticulo GROUP BY a.Id, a.Nombre, a.Descripcion");
 
-                using (SqlDataReader reader = datos.EjecutarLectura())
-                {
-                    List<Articulos> premios = new List<Articulos>();
+                // Ejecutar la lectura de datos
+                SqlDataReader lector = datos.EjecutarLectura();
 
-                    while (reader.Read())
-                    {
-                        Articulos articulo = new Articulos
-                        {
-                            IdArticulo = Convert.ToInt32(reader["PremioID"]),
-                            Nombre = reader["Nombre"].ToString(),
-                            Descripcion = reader["Descripcion"].ToString(),
-                            precio = Convert.ToDecimal(reader["Precio"]),
-                            imagen = reader["ImagenUrl"].ToString()
-                        };
-                        premios.Add(articulo);
-                    }
-
-                    rptPremios.DataSource = premios;
-                    rptPremios.DataBind();
-                }
+                // Cargar los datos al repeater
+                rptPremios.DataSource = lector;
+                rptPremios.DataBind();
             }
             catch (Exception ex)
             {
-                lblMessage2.Text = "Ocurrió un error al cargar los premios: " + ex.Message;
+                // Manejar errores
+                Console.WriteLine("Error al cargar los productos: " + ex.Message);
             }
             finally
             {
                 datos.cerrarConexion();
+            }
+        }
+
+        protected void btnSeleccionarPremio_Click(object sender, EventArgs e)
+        {
+            List<int> premiosSeleccionados = new List<int>();
+
+            // Recorrer los ítems del Repeater
+            foreach (RepeaterItem item in rptPremios.Items)
+            {
+                // Obtener el checkbox y el HiddenField del item actual
+                CheckBox chkSeleccionar = (CheckBox)item.FindControl("chkSeleccionar");
+                HiddenField hfPremioID = (HiddenField)item.FindControl("hfPremioID");
+
+                // Verificar si el checkbox está marcado
+                if (chkSeleccionar != null && chkSeleccionar.Checked)
+                {
+                    // Obtener el ID del premio y agregarlo a la lista
+                    int premioID = int.Parse(hfPremioID.Value);
+                    premiosSeleccionados.Add(premioID);
+                }
+            }
+
+            // Verificar si se ha seleccionado algún premio
+            if (premiosSeleccionados.Count > 0)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "Premios seleccionados: " + string.Join(", ", premiosSeleccionados);
+                // Aquí podrías realizar la lógica adicional para procesar los premios seleccionados,
+                // como guardar los premios en una base de datos o realizar alguna acción con ellos.
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "No has seleccionado ningún premio.";
             }
         }
     }
