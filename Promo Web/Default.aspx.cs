f0683f41fb80ada087418a04b9d37d8dc67a834e
FrancoBonzi <bon.franco12@gmail.com>
2024-10-06 19:56:36 -0300

    Commit: Default.aspx y Default.aspx.cs

---
+++ Promo Web/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using negocio;
+
+namespace Promo_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnComenzar_Click(object sender,EventArgs e)
+        {
+            string script = "<script type='text/javascript'>setTimeout(function(){window.location.href='Vouchers.apsx';},2000;</script>";
+
+            ClientScript.RegisterStartupScript(this.GetType(), "Redirect", script);
+        }
+    }
+}
